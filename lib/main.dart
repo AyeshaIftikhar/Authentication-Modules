7de470b44bea3a66d36c2ded506a7c0c4bcb0b2f
Ayesha Iftikhar <seAyeshaiftikhar@gmail.com>
2021-07-01 15:14:09 +0500

    url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'configure_noweb.dart' if (dart.library.html) 'configure_web.dart';
 import './Constants/configuration_ids.dart';
 import './my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  configureApp();
   if (kIsWeb) {
     // initialiaze the facebook javascript SDK
     FacebookAuth.i.webInitialize(
